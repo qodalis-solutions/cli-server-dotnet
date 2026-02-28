9869adc2acb7f626fbed7609bc8275c13706941f
Nicolae Lupei <nicolae.lupei@acconomy.com>
2026-02-28 17:19:45 +0200

    refactor: clean up processors to match web-cli conventions
    - Rename to Cli{Feature}CommandProcessor pattern (matching TypeScript side)
    - Remove GuidCommandProcessor (duplicates client-side @qodalis/cli-guid)
    - Add CliCommandAuthor concrete class and DefaultLibraryAuthor constant
    - Make base class Author virtual with default (instead of abstract with null!)
    - Clean up processor boilerplate

--- src/Qodalis.Cli/CliCommandProcessor.cs
+++ src/Qodalis.Cli/CliCommandProcessor.cs
@@ -1,4 +1,4 @@
-﻿using Qodalis.Cli.Abstractions;
+using Qodalis.Cli.Abstractions;
 
 namespace Qodalis.Cli;
 
@@ -6,9 +6,9 @@ public abstract class CliCommandProcessor : ICliCommandProcessor
 {
     public abstract string Command { get; set; }
     public abstract string Description { get; set; }
-    public abstract ICliCommandAuthor Author { get; set; }
-    public abstract bool? AllowUnlistedCommands { get; set; }
-    public abstract bool? ValueRequired { get; set; }
+    public virtual ICliCommandAuthor Author { get; set; } = DefaultLibraryAuthor.Instance;
+    public virtual bool? AllowUnlistedCommands { get; set; }
+    public virtual bool? ValueRequired { get; set; }
     public virtual string Version { get; set; } = "1.0.0";
 
     public virtual IEnumerable<ICliCommandProcessor>? Processors { get; set; }
